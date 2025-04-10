2429e0948c8c9c0085ac39f721fb12c44c9fc3c6
khashayar <aripastudio44@gmail.com>
2025-04-10 06:27:52 -0700

    Ui MainPage Two commit

--- AripaStudioHub/Views/MainView.axaml.cs
+++ AripaStudioHub/Views/MainView.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace AripaStudioHub.Views;
 
@@ -7,5 +8,50 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void Btn_main_page_border_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Border_MainPage.IsVisible = true;
+        Border_GamesAP.IsVisible = false;
+        Border_SocialMediaLinks.IsVisible = false;
+        Border_SoftWareAP.IsVisible = false;
+        Border_Setting.IsVisible = false;
+    }
+
+    private void Btn_Social_Media_Links_page_border_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Border_MainPage.IsVisible = false;
+        Border_GamesAP.IsVisible = false;
+        Border_SocialMediaLinks.IsVisible = true;
+        Border_SoftWareAP.IsVisible = false;
+        Border_Setting.IsVisible = false;
+    }
+
+    private void Btn_Game_page_border_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Border_MainPage.IsVisible = false;
+        Border_GamesAP.IsVisible = true;
+        Border_SocialMediaLinks.IsVisible = false;
+        Border_SoftWareAP.IsVisible = false;
+        Border_Setting.IsVisible = false;
+    }
+
+    private void Btn_SoftWare_page_border_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Border_MainPage.IsVisible = false;
+        Border_GamesAP.IsVisible = false;
+        Border_SocialMediaLinks.IsVisible = false;
+        Border_SoftWareAP.IsVisible = true;
+        Border_Setting.IsVisible = false;
+    }
+
+    private void Btn_Setting_page_border_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Border_MainPage.IsVisible = false;
+        Border_GamesAP.IsVisible = false;
+        Border_SocialMediaLinks.IsVisible = false;
+        Border_SoftWareAP.IsVisible = false;
+        Border_Setting.IsVisible = true;
     }
 }
