0eda22f7ab915008d85fdceb96d9785707c95698
khashayar <aripastudio44@gmail.com>
2025-04-10 05:33:21 -0700

    AddProject commit

---
+++ AripaStudioHub/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AripaStudioHub.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    
+}
---
+++ AripaStudioHub/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AripaStudioHub.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
